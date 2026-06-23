7f3b8908b47df6758c12b79a1f6b32f6a841d9b4
muhammadzainali92 <muhammadzainiali92@gmail.com>
2026-06-23 10:14:40 -0700

    Even And Odd Reasoning

--- class4.dart
+++ class4.dart
@@ -1,4 +1,5 @@
 void main() {
   print("Just For Fun");
   print("Dart is a great programming language.");
+  print("I enjoy learning Dart and building applications with it.");
 }
---
+++ even_odd.dart
@@ -0,0 +1,8 @@
+void main() {
+  int number = 23;
+  if (number % 2 == 0) {
+    print("number is even");
+  } else {
+    print("number is odd");
+  }
+}
